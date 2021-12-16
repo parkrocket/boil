7be2bddd052a40f91f42ec474c6dfad8008709fd
parkrocket <mongkys@naver.com>
2021-12-16 17:44:59 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -3,19 +3,41 @@ const app = express();
 const port = 5000;
 
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
+
 mongoose
-  .connect(
-    "mongodb+srv://parkrocket:4523105a@parkrocket.k2kwl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB Connet"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("안녕하세요 :)");
+});
+
+app.post("/register", (req, res) => {
+  // 회원가입 필요한 정보를 클라이언트에서 가져오면
+  // 그 데이터를 데이터베이스에 INSERT
+  const user = new User(req.body);
+
+  //mongodb
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
